c35a7019af57bd12039b9c3ac900b5572c65fdeb
brendo <brendothierry41@gmail.com>
2022-05-19 16:36:52 -0300

    App react-native

---
+++ App.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import {View, StyleSheet, Text, Image, TouchableOpacity} from 'react-native';
+
+class App extends Component{
+
+constructor(props){
+  super(props);
+  this.state={
+    numero: 0,
+    botao: 'VAI'
+  };
+
+  this.timer = null;
+  this.vai = this.vai.bind(this);
+  this.limpar = this.limpar.bind(this);
+}
+
+vai() {
+  console.log(this.timer)
+  if(this.timer != null){
+    clearInterval(this.timer);
+    this.timer = null;
+
+    this.setState({botao: 'VAI'})
+  }else{
+    console.log(this.timer + "2")
+    this.timer = setInterval( ()=> {
+      this.setState({numero: this.state.numero + 0.1})
+      }, 100); 
+
+      this.setState({botao: 'PARAR'})
+  }
+}
+
+limpar() {
+
+}
+
+
+
+  render(){
+    return(
+      <View style={styles.container}>
+      <Image 
+      source={require('./src/cronometro.png')}
+      style={styles.cronometro}/>
+      <Text style={styles.timer}>{this.state.numero.toFixed(1)} </Text>
+      
+      <View style={styles.btnArea}>
+        
+        <TouchableOpacity style={styles.btn} onPress={this.vai}>
+          <Text style={styles.btnTexto}> {this.state.botao} </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn} onPress={this.limpar}>
+          <Text style={styles.btnTexto}>LIMPAR</Text>
+        </TouchableOpacity>
+
+      </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor: '#00aeef'
+  },
+  timer:{
+    marginTop: -160,
+    color: '#FFF',
+    fontSize: 65,
+    fontWeight: 'bold'
+  },
+  btnArea:{
+    flexDirection: 'row',
+    marginTop: 70,
+    height: 40
+  },
+  btn:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    height: 40,
+    margin: 17,
+    borderRadius: 9
+  },
+  btnTexto:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#00aeef'
+  }
+
+})
+
+export default App;
